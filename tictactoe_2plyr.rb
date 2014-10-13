42bf4d67bb209bd4bbd273cca100c6588575a2c8
Jisu Kim <jisu.kim@flatironschool.com>
2014-10-13 17:22:54 -0400

    require the separate methods file

--- tictactoe_2plyr.rb
+++ tictactoe_2plyr.rb
@@ -1,42 +1,13 @@
 #Tic Tac Toe 2 Players
-
-def print_grid(grid)
-  puts "#{grid[0]}" +"  |  " + "#{grid[1]}" + "  |  " + "#{grid[2]}"
-  puts "______________"
-  puts "#{grid[3]}" +"  |  " + "#{grid[4]}" + "  |  " + "#{grid[5]}"
-  puts "______________"
-  puts "#{grid[6]}" +"  |  " + "#{grid[7]}" + "  |  " + "#{grid[8]}"
-end
-
-def check_winner?(grid)
- if (grid[0]!=' ' && ((grid[0]==grid[1]) && (grid[1]==grid[2])))
-    true
- elsif (grid[0]!=' ' && ((grid[0]==grid[4]) && (grid[4]==grid[8])))
-    true
- elsif (grid[0]!=' ' && ((grid[0]==grid[3]) && (grid[3]==grid[6])))
-    true
- elsif (grid[3]!=' ' && ((grid[3]==grid[4]) && (grid[4]==grid[5])))
-    true
- elsif (grid[6]!=' ' && ((grid[6]==grid[7]) && (grid[7]==grid[8])))
-    true
- elsif (grid[1]!=' ' && ((grid[1]==grid[4]) && (grid[4]==grid[7])))
-    true
- elsif (grid[2]!=' ' && ((grid[2]==grid[5]) && (grid[5]==grid[8])))
-    true
- elsif (grid[2]!=' ' && ((grid[2]==grid[4]) && (grid[4]==grid[6])))
-    true 
- else
-    false
- end
-end
+require_relative 'methods.rb'
 
 grid = []
 counter=9 #when counter reaches 0 it is a tie
 
-0.upto(8) {|x| grid[x] = x} #show initial grid with location values
+(0..8).each {|x| grid[x] = x} #show initial grid with location values
 print_grid(grid)
 
-0.upto(8) {|x| grid[x] = ' '} #re-assign each slot to a black space
+(0..8).each {|x| grid[x] = ' '} #re-assign each slot to a black space
                               #keeps the alignment of the grid
 game_over = false
 
